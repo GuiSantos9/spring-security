21c2da3d9a869f60592b67579540d309eb4d65cc
unknown <guilhermeaugusto923@gmail.com>
2026-05-26 19:43:57 -0300

    mudanças na pasta security

--- src/main/java/com/security/br/securities/SecurityConfiguration.java
+++ src/main/java/com/security/br/securities/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.security.br.securities;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,10 +14,15 @@ import org.springframework.stereotype.Controller;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.GET, "/pessoas").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/pessoas").permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/pessoas/**").permitAll()
+                                .requestMatchers(HttpMethod.PUT, "/pessoas/**").permitAll())
                 .build();
     }
 
